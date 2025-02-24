cf3674fd1d7d558a2eb47f55958d99ed2c840c6d
Avish Parekh <avishparekh06@gmail.com>
2025-02-24 00:25:28 -0600

    basic page and navbar setup

---
+++ src/pages/About.jsx
@@ -0,0 +1,13 @@
+function About() {
+    const textStyle = {
+        textAlign: "center",
+        paddingTop: "200px"
+    };
+    return(
+        <>
+            <h1 style={textStyle}>About</h1>
+        </>
+    )
+}
+
+export default About;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+function Home() {
+    const textStyle = {
+        textAlign: "center",
+        paddingTop: "200px"
+    };
+    return(
+        <>
+            <h1 style={textStyle}>HOME</h1>
+        </>
+    )
+}
+
+export default Home;
